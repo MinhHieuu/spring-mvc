ac09e24a2a6dca0283220ba3d72fcd34497b0c59
Minh Hieu <hieuassz04@gmail.com>
2025-10-15 10:01:17 +0700

    'int'

---
+++ src/main/java/vn/uhie/ieuhsshop/IeuhsshopApplication.java
@@ -0,0 +1,13 @@
+package vn.uhie.ieuhsshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IeuhsshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IeuhsshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/uhie/ieuhsshop/IeuhsshopApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.uhie.ieuhsshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IeuhsshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
